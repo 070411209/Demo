c05d380a6ef898d9a7475a2f89733c49eb4f5340
王包东 <wangbaodong@deepway.ai>
2022-10-27 19:43:33 +0800

    fff

---
+++ fifo/main.cpp
@@ -0,0 +1,32 @@
+// inserting into a vector
+#include <iostream>
+#include <vector>
+
+int main ()
+{
+  std::vector<int> myvector (4,100);
+  std::vector<int>::iterator it;
+
+  it = myvector.begin();
+  it = myvector.insert ( it , 200 );
+
+  myvector.insert (it,300);
+
+
+  myvector.pop_back();
+//   // "it" no longer valid, get a new one:
+//   it = myvector.begin();
+
+//   std::vector<int> anothervector (2,400);
+//   myvector.insert (it+2,anothervector.begin(),anothervector.end());
+
+//   int myarray [] = { 501,502,503 };
+//   myvector.insert (myvector.begin(), myarray, myarray+3);
+
+  std::cout << "myvector contains:";
+  for (it=myvector.begin(); it<myvector.end(); it++)
+    std::cout << ' ' << *it;
+  std::cout << '\n';
+
+  return 0;
+}
